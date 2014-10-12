9cc00b4883c746e752d132171c3a061d7abac686
WilliamDevine <williamdevine@mac.com>
2014-10-12 12:22:17 -0700

    Initial Commit

---
+++ Tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr
+//
+//  Created by WilliamDevine on 10/12/14.
+//  Copyright (c) 2014 WilliamDevine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
